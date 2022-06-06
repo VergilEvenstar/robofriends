f3fc1edc8a77285982fd5d90207de3195903a0c0
Vergil Evenstar <thefrozenwatcher95@gmail.com>
2022-06-06 22:04:36 +0700

    Implementing Hook (useState and useEffect)

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,35 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import CardList from "../Components/CardList";
 import SearchBox from "../Components/SearchBox";
 import Scroll from '../Components/Scroll';
 import ErrorBoundry from "../Components/ErrorBoundry";
 import './App.css';
 
-class App extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchField : ''
-        }
-    }
+function App() {
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
+    const [count, setCount] = useState(0);
 
-    componentDidMount(){
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => {
-            return response.json();
-        })
-        .then(users => {
-            this.setState({robots: users});
-        });
+            .then(response => {
+                return response.json();
+            })
+            .then(users => {
+                setRobots(users);
+            });
+    }, [count]);
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value})
-    }
-
-    render(){
-        const {robots , searchField} = this.state;
+    {
         const filteredRobots = robots.filter(robot =>{
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
@@ -38,7 +33,8 @@ class App extends React.Component {
             (
                 <div className='tc'>
                     <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchField={this.searchField} searchChange={this.onSearchChange} />
+                    <button onClick={() => setCount(count + 1)}>Counter.</button> <span>{count}</span>
+                    <SearchBox searchField={searchField} searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
                             <CardList robots={filteredRobots} />
